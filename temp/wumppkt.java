c5e55911182c334d70575249a703a6d1b3eee3e1
Naser Sharifi <sharifinaser@gmail.com>
2013-12-16 20:43:29 -0600

    First submission

---
+++ temp/wumppkt.java
@@ -0,0 +1,309 @@
+// The following implements the packet formats for BUMP and HUMP.
+// Each individual packet type derives from class BASE, containing
+// protocol and opcode fields only; we don't really use this inheritance
+// hierarchy though.
+
+// packets can be constructed with the applicable constructors;
+// each ctor requires parameters for the necessary fields.
+// when possible, there is a "convenience" ctor setting proto = BUMPPROTO.
+
+// The "raw" packet format, as sent and received via DatagramSocket,
+// is byte[].  Packets (at least those that one might *receive*)
+// can be constructed from a byte[]. For DATA packets, we also need
+// to specify the length of the packet, not necessarily the same as
+// the length of the byte[] buffer.
+
+// All packet classes also have a write() member function that
+// writes out the packet fields into a byte[], for sending.
+
+//import java.lang.*;     //pld
+//import java.net.*;      //pld
+//import java.lang.System.*;
+import java.io.*;
+
+public class wumppkt {
+
+    public static final short BUMPPROTO = 1;
+    public static final short HUMPPROTO = 2;
+    public static final short CHUMPPROTO= 3;
+
+    public static final short REQop = 1;
+    public static final short DATAop= 2;
+    public static final short ACKop = 3;
+    public static final short ERRORop=4;
+    public static final short HANDOFFop=5;
+
+    public static final short SERVERPORT = 4715;
+    public static final short SAMEPORT   = 4716;
+
+    public static final int   INITTIMEOUT = 3000;   // milliseconds
+    public static final int   SHORTSIZE = 2;            // in bytes
+    public static final int   INTSIZE   = 4;
+    public static final int   BASESIZE  = 2;
+    public static final int   MAXDATASIZE=512;
+    public static final int   DHEADERSIZE = BASESIZE + SHORTSIZE + INTSIZE; // DATA header size
+    public static final int   MAXSIZE  = DHEADERSIZE + MAXDATASIZE;
+
+    public static final int   EBADPORT  =1;  /* packet from wrong port */
+    public static final int   EBADPROTO =2;  /* unknown protocol */
+    public static final int   EBADOPCODE=3;  /* unknown opcode */
+    public static final int   ENOFILE   =4;  /* REQ for nonexistent file */
+    public static final int   ENOPERM   =5;  /* REQ for file with wrong permissions */
+
+
+static int proto(byte[] buf) {
+    return  buf[0];
+}
+
+static int opcode(byte[] buf) {
+    return buf[1];
+}
+
+    public static void w_assert(boolean cond, String s) {
+        if (cond) return;
+        System.err.println("assertion failed: " + s);
+        java.lang.System.exit(1);
+    }
+
+//************************************************************************
+
+public class BASE { //implements Externalizable {
+// don't construct these unless the buffer has length >=4!
+
+// the data:
+    private byte  _proto;
+    private byte  _opcode;
+
+    //---------------------------------
+
+    public BASE(int proto, int opcode) {
+        //super();              // call to base ctor
+        _proto = (byte) proto;
+        _opcode = (byte) opcode;
+    }
+
+    public BASE(byte[] buf) {        // constructs pkt out of packetbuf
+    }
+
+    public BASE() {}                 // packet ctors do all the work!
+
+    public byte[] write() {         // not used
+        return null;
+    }
+
+    public int size() {
+        return BASESIZE;
+    }
+
+    public int proto() {return _proto;}
+    public int opcode() {return _opcode;}
+}
+
+//*******************
+
+public class REQ extends BASE {
+
+    private short  _winsize;
+    private String _filename;
+
+    //---------------------------------
+
+    public REQ(int proto, int winsize, String filename) {
+        super(proto, REQop);
+        _winsize = (short) winsize;
+        _filename = filename;
+    }
+
+    public REQ(int winsize, String filename) {
+        this(BUMPPROTO, winsize, filename);
+    }
+
+    public REQ(byte[] buf) {            // not complete but not needed
+        //super(BUMPPROTO, REQop);
+    }
+
+    public byte[] write() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(size());
+        DataOutputStream dos = new DataOutputStream(baos);
+        try {
+            //writeExternal(dos);
+            dos.writeByte(super.proto());
+            dos.writeByte(super.opcode());
+            dos.writeShort(_winsize);
+            dos.writeBytes(_filename);
+            dos.writeByte(0);
+            return baos.toByteArray();
+            } catch (IOException ioe) {
+                System.err.println("BASE packet output conversion failed");
+                return null;
+            }
+        //return null;
+    }
+
+    public int size() {
+        return super.size() + SHORTSIZE + _filename.length() + 1;
+    }
+
+    public String filename() {return _filename;}
+}
+
+//*******************
+
+public class ACK extends BASE {
+
+    private int _blocknum;
+
+   //---------------------------------
+
+   public ACK(int blocknum) {
+        this(BUMPPROTO, blocknum);
+    }
+
+    public ACK(short proto, int blocknum) {
+        super(proto, ACKop);
+        _blocknum = blocknum;
+    }
+
+    public int blocknum() {return _blocknum;}
+    public void setblock(int blocknum) {_blocknum = blocknum;}
+
+    public byte[] write() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(size());
+        DataOutputStream dos = new DataOutputStream(baos);
+        try {
+            //writeExternal(dos);
+            dos.writeByte(super.proto());
+            dos.writeByte(super.opcode());
+            dos.writeShort(0);          // padding
+            dos.writeInt(_blocknum);
+            return baos.toByteArray();
+            } catch (IOException ioe) {
+                System.err.println("ACK packet output conversion failed");
+                return null;
+            }
+     }
+
+    public int size() {
+        return super.size() + SHORTSIZE + INTSIZE;
+    }
+
+    public ACK(byte[] buf) {}       // not complete but not needed
+}
+
+//*******************
+
+public class DATA extends BASE {
+
+    private int _blocknum;
+    private byte[] _data;
+
+    //---------------------------------
+
+    public DATA(int proto, int blocknum, byte[] data) {
+        super(proto, DATAop);
+        _blocknum = blocknum;
+        _data = data;
+    }
+
+    public DATA(int proto, int blocknum, byte[] data, int len) {
+        super(proto, DATAop);
+        _blocknum = blocknum;
+        _data = data;
+    }
+
+    public DATA(byte[] buf, int bufsize) {
+        this(BUMPPROTO, buf, bufsize);
+    }
+
+    // for building a DATA out of incoming buffer:
+    public DATA(int proto, byte[] buf, int bufsize) {
+        super(proto, DATAop);
+        ByteArrayInputStream bais = new ByteArrayInputStream(buf, 0, bufsize);
+        DataInputStream dis = new DataInputStream(bais);
+        try {
+            int p = dis.readByte();
+            w_assert(p==proto, "Expecting proto " + proto + ", got " + p);
+            int o = dis.readByte();
+            w_assert(o==DATAop, "Expecting opcode=DATA, got " + o);
+            int pad=dis.readShort();
+            _blocknum = (dis.readInt());
+            _data  = new byte[dis.available()];
+            dis.read(_data);
+        } catch (IOException ioe) {
+            System.err.println("DATA packet conversion failed");
+            return;
+        }
+    }
+
+    public DATA(int proto) {            // for creating "empty" DATA objects
+        super(proto, DATAop);
+        _blocknum = 0;
+        _data = new byte[MAXDATASIZE];
+    }
+
+    public DATA() { this(BUMPPROTO); }
+
+    public int blocknum() {return _blocknum;}
+    public byte[] data() {return _data;}
+
+    public byte[] write() {     // not complete but not needed
+        return null;
+    }
+
+    public int size() {
+        return super.size() + SHORTSIZE + INTSIZE + _data.length;
+    }
+}
+
+//*******************
+
+public class ERROR extends BASE {
+
+    private short _errcode;
+
+    //---------------------------------
+    public ERROR(short proto, short errcode) {
+		super(proto, ERRORop);
+		_errcode = errcode;
+	}
+
+    public short errcode() {return _errcode;}
+
+    public byte[] write() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(size());
+        DataOutputStream dos = new DataOutputStream(baos);
+        try {
+            //writeExternal(dos);
+            dos.writeByte(super.proto());
+            dos.writeByte(super.opcode());
+            dos.writeShort(_errcode);
+            return baos.toByteArray();
+        } catch (IOException ioe) {
+            System.err.println("ERROR packet output conversion failed");
+            return null;
+        }
+    }
+
+    public ERROR(byte[] buf) {this(BUMPPROTO, buf);}
+
+    public ERROR(int proto, byte[] buf) {
+        super(proto, DATAop);
+        int opcode = wumppkt.this.opcode(buf);
+        w_assert(opcode == ERRORop, "Expecting opcode=ERROR, got " + opcode);
+        w_assert(proto == wumppkt.this.proto(buf), "Expecting proto="+proto);
+        w_assert(buf.length >= BASESIZE + SHORTSIZE, "bad ERROR pkt size of " + buf.length);
+        ByteArrayInputStream bais = new ByteArrayInputStream(buf, 0, buf.length);
+        DataInputStream dis = new DataInputStream(bais);
+        try {
+            int p = dis.readByte();
+            int o = dis.readByte();
+            _errcode = dis.readShort();
+        } catch (IOException ioe) {
+            System.err.println("ERROR packet conversion failed");
+            return;
+        }
+    };
+
+    public int size() {return super.size() + SHORTSIZE;}
+}
+}
--- wump/wclient.java
+++ wump/wclient.java
@@ -2,18 +2,15 @@
     WUMP (specifically BUMP) in java. starter file
  */
 import java.lang.*; //pld
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
+import java.net.*; //pld
 import java.io.*;
 //import wumppkt;         // be sure wumppkt.java is in your current directory
 //import java.io.Externalizable;
 
 public class wclient {
-	public static final int MIN_SIZE = 512;			
+
+	public static final int FULL_PKT_SIZE = 512;
+	public static final int DALLY_MAX = 4;
 	
 	static wumppkt wp = new wumppkt(); // stupid inner-class nonsense
 
@@ -21,191 +18,198 @@ public class wclient {
 	// ============================================================
 
 	static public void main(String args[]) {
+		int destport = wumppkt.SERVERPORT;
+		destport = wumppkt.SAMEPORT; // 4716; server responds from same port
+		String filename = "vanilla";
+		String desthost = "ulam2.cs.luc.edu";
+		int winsize = 1;
 
-		int destPort = wumppkt.SERVERPORT; 			// Destination Port number
-		String destHostname = "ulam2.cs.luc.edu";	// Destination hostname
-		String filename = "vanilla";				// The file at destination host
-		int winSize = 1;							// Window size
-
-		// Set parameters based on commandline arguments (if any)
 		if (args.length > 0)
 			filename = args[0];
 		if (args.length > 1)
-			winSize = Integer.parseInt(args[1]);
+			winsize = Integer.parseInt(args[1]);
 		if (args.length > 2)
-			destHostname = args[2];
+			desthost = args[2];
 
-		DatagramSocket socket = null;
+		DatagramSocket s;
 		try {
-			socket = new DatagramSocket();
+			s = new DatagramSocket();
 		} catch (SocketException se) {
 			System.err.println("no socket available");
 			return;
 		}
 
 		try {
-			socket.setSoTimeout(wumppkt.INITTIMEOUT); // time in milliseconds
+			s.setSoTimeout(wumppkt.INITTIMEOUT); // time in milliseconds
 		} catch (SocketException se) {
-			System.err.println("socket exception: timeout setting failed!");
+			System.err.println("socket exception: timeout not set!");
 		}
 
-//		if (args.length != 2) {
-//			System.err.println("usage: wclient filename  [winsize [hostname]]");
-//			// exit(1);
-//		}
-
-		// DNS lookup for the destination
-		InetAddress destInet = null;
-		System.err.print("Looking up address of " + destHostname + "...");
+		// DNS lookup
+		InetAddress dest;
+		System.err.print("Looking up address of " + desthost + "...");
 		try {
-			destInet = InetAddress.getByName(destHostname);
+			dest = InetAddress.getByName(desthost);
 		} catch (UnknownHostException uhe) {
-			System.err.println("unknown host: " + destHostname);
+			System.err.println("unknown host: " + desthost);
 			return;
 		}
 		System.err.println(" got it!");
 
 		// build REQ & send it
-		wumppkt.REQ request = wp.new REQ(wumppkt.BUMPPROTO, winSize, filename); // ctor
+		wumppkt.REQ req = wp.new REQ(wumppkt.BUMPPROTO, winsize, filename); // ctor
 																			// for
 																			// REQ
 
-		System.err.println("req size = " + request.size() + 
-				", filename=" + request.filename());
+		System.err.println("req size = " + req.size() + ", filename="
+				+ req.filename());
 
-		DatagramPacket requestDatagram = 
-				new DatagramPacket(request.write(), request.size(), destInet, destPort);
-		
+		DatagramPacket reqDG = new DatagramPacket(req.write(), req.size(),
+				dest, destport);
 		try {
-			socket.send(requestDatagram);
+			s.send(reqDG);
 		} catch (IOException ioe) {
 			System.err.println("send() failed");
-			ioe.printStackTrace();
 			return;
 		}
 
-		// now receive the response
-		DatagramPacket responseDatagram = 
-				new DatagramPacket(new byte[wumppkt.MAXSIZE], wumppkt.MAXSIZE); 
-		// we don't set the address here!
-		
-		DatagramPacket ackDatagram = new DatagramPacket(new byte[0], 0);
-		ackDatagram.setAddress(destInet);
-		ackDatagram.setPort(destPort); // this is wrong for wumppkt.SERVERPORT version
+		// ============================================================
 
-		int expectedBlock = 1;
-		long startTime = System.currentTimeMillis();
-		long sendTime = startTime;
+		// now receive the response
+		DatagramPacket replyDG // we don't set the address here!
+			= new DatagramPacket(new byte[wumppkt.MAXSIZE], wumppkt.MAXSIZE);
+		DatagramPacket ackDG = new DatagramPacket(new byte[0], 0);
+		ackDG.setAddress(dest);
+		ackDG.setPort(destport); // this is wrong for wumppkt.SERVERPORT version
+
+		int expected_block = 1;
+		long starttime = System.currentTimeMillis();
+		long sendtime = starttime;
 
 		wumppkt.DATA data = wp.new DATA();
 		wumppkt.ACK ack = wp.new ACK(0);
-
 		int proto; // for proto of incoming packets
 		int opcode;
 		int length;
 
 		// ============================================================
-		int serverPort = 0;
+		int expectedPort = 0;
+		int timeoutCount = 0;
+		boolean isDally = false;
 		while (true) {
 			// get packet
-			try {
-				socket.receive(responseDatagram);
-			} catch (SocketTimeoutException ste) {
+            try {
+                s.receive(replyDG);
+            }
+            catch (SocketTimeoutException ste) {
 				System.err.println("hard timeout");
+				timeoutCount++;
+				// Check for dally
+				if (isDally && timeoutCount >= DALLY_MAX) {
+	            	System.err.println("Exiting after " + DALLY_MAX + " timeouts");
+	            	break;
+				}
 				// what do you do here??
 				continue;
-			} catch (IOException ioe) {
-				System.err.println("receive() failed");
-				return;
+            }
+            catch (IOException ioe) {
+                System.err.println("receive() failed");
+                return;
+            }
+
+            byte[] replybuf = replyDG.getData();
+            proto = wumppkt.proto(replybuf);
+            opcode = wumppkt.opcode(replybuf);
+            length = replyDG.getLength();
+
+            /* The new packet might not actually be a DATA packet.
+             * But we can still build one and see, provided:
+             *   1. proto =   wumppkt.BUMPPROTO
+             *   2. opcode =  wumppkt.DATAop
+             *   3. length >= wumppkt.DHEADERSIZE
+             */
+
+			if (  proto == wumppkt.BUMPPROTO
+			      && opcode == wumppkt.DATAop
+			      && length >= wumppkt.DHEADERSIZE)
+			{
+            		data = wp.new DATA(replyDG.getData(), length);
+            } else {
+            		data = null;
 			}
-			
-			
-			// latch on to port, if block == 1
-			if (data.blocknum() == 1) {
-				serverPort = responseDatagram.getPort();
-			}
-			
-			// Check for host
-			if (!(responseDatagram.getAddress().equals(destInet))) {
-				System.err.println("Incorrect destination address");
-				return;
-			}
-			// Check the port; 
-			if (responseDatagram.getPort() != serverPort) {
+
+			// Check IP address
+			if (!replyDG.getAddress().equals(dest)) {
+				System.err.println("Packet received from invalid host");
 				continue;
 			}
-			// if size < 512 exit
-			if (data.size() < MIN_SIZE) {
-				System.err.println("Reached end of transfer... program exiting...");
-				break;
+
+			// Latch on the port for DATA[1]
+			if (data.blocknum() == 1) {
+				expectedPort = replyDG.getPort();
+			}
+			// Check if data is from the right port send an error packet
+			if (replyDG.getPort() != expectedPort) {
+				System.err.println("Packet received from invalid port");
+				// Send error packet
+				wumppkt.ERROR err = wp.new ERROR(wumppkt.BUMPPROTO, (short) wumppkt.EBADPORT);
+				DatagramPacket errDG = new  DatagramPacket(err.write(), err.size());
+				try {
+					s.send(errDG);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				continue;
 			}
 			
-			// Check opcode
-			
-			
-			
-			
-			byte[] responseBuffer = responseDatagram.getData();
-			proto = wumppkt.proto(responseBuffer);
-			opcode = wumppkt.opcode(responseBuffer);
-			length = responseDatagram.getLength();
-
-			/*
-			 * The new packet might not actually be a DATA packet. But we can
-			 * still build one and see, provided: 1. proto = wumppkt.BUMPPROTO
-			 * 2. opcode = wumppkt.DATAop 3. length >= wumppkt.DHEADERSIZE
-			 */
-			
-			if (proto == wumppkt.BUMPPROTO && opcode == wumppkt.DATAop && 
-					length >= wumppkt.DHEADERSIZE) {
-				data = wp.new DATA(responseDatagram.getData(), length);
-			} else {
-				data = null;
-			}
-
 			// the following seven items we can print always
-			System.err.print("rec'd packet: len=" + length);
+			System.out.print("Received packet: { ");
+			System.err.println("<len=" + length);
 			System.err.print("; proto=" + proto);
 			System.err.print("; opcode=" + opcode);
-			System.err.print("; src=(" + responseDatagram.getAddress().getHostAddress()
-					+ "/" + responseDatagram.getPort() + ")");
-			System.err.print("; time="
-					+ (System.currentTimeMillis() - startTime));
+			System.err.print("; src=(" + replyDG.getAddress().getHostAddress() + 
+					"/" + replyDG.getPort()+ ")");
+			System.err.print("; time=" + (System.currentTimeMillis()-starttime));
 			System.err.println();
 
-			if (data == null)
+			if (data==null)
 				System.err.println("         packet does not seem to be a data packet");
 			else {
-				System.err.println("         DATA packet blocknum = " + 
-						data.blocknum());
+				System.err.println("\t\tDATA packet blocknum = " + data.blocknum() + "> ");
+				System.out.print("[");
 				System.out.write(data.data(), 0, data.size() - wumppkt.DHEADERSIZE);
+				System.out.println("] }");
 			}
+            // The following is for you to do:
+            // check port, packet size, type, block, etc
+            // latch on to port, if block == 1
 
-			
-			
-			
+            // send ack
+            ack = wp.new ACK(wumppkt.BUMPPROTO, expected_block);
+            ackDG.setData(ack.write());
+            ackDG.setLength(ack.size());
+            ackDG.setPort(expectedPort); // Set the right port
+            try {s.send(ackDG);}
+            catch (IOException ioe) {
+                System.err.println("send() failed");
+                return;
+            }
+            sendtime = System.currentTimeMillis();
 
-			// send ack
-			ack = wp.new ACK(wumppkt.BUMPPROTO, expectedBlock);
-			ackDatagram.setData(ack.write());
-			ackDatagram.setLength(ack.size());
-			ackDatagram.setPort(serverPort); // GTUX
-			try {
-				socket.send(ackDatagram);
-			} catch (IOException ioe) {
-				System.err.println("send() failed");
-				return;
+			// Check if it's last packet and possibly set dally
+			if ((data.size() - wumppkt.DHEADERSIZE) < FULL_PKT_SIZE) {
+				isDally = true;
 			}
-			sendTime = System.currentTimeMillis();
+			
 
-			
-			
-			// if it passes all the checks:
-			// write data, increment expected_block
-			// exit if data size is < 512
-			
-						
-
+	    // if it passes all the checks:
+            //write data, increment expected_block
+	    // exit if data size is < 512		
+            expected_block++;
 		} // while
+		
+		// Close the socket
+		s.close();
 	}
 }
